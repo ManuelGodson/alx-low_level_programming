dd4742e0fd634c4e8509d94410066abdfe51031b
ManuelGodsod <dynamictunes@yahoo.com>
2022-09-10 09:28:33 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	if (a == 'e' || a == 'q')
+		{
+			continue;
+		}
+	putchar(a)
+	}
+	putchar('\n');
+	return (0);
+}
+
+
