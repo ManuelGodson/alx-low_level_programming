4b438f2af2ad8fd240466b46bac9822fb0267f58
ManuelGodsod <dynamictunes@yahoo.com>
2022-09-14 09:17:11 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,18 +13,18 @@ void times_table(void)
 
 	for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
+		_putchar(',');
+		_putchar(' ');
 
-			prod = num * mult;
+		prod = num * mult;
 
-			if (prod <= 9)
-			_putchar(' ');
-			else
-			_putchar((prod / 10) + '0');
+		if (prod <= 9)
+		_putchar(' ');
+		else
+		_putchar((prod / 10) + '0');
 
-			_putchar((prod % 10) + '0');
+		_putchar((prod % 10) + '0');
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
